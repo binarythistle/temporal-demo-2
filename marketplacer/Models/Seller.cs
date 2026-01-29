7e988cd37b25b485139894726f35cde5164c03d7
binarythistle <les.jackson@binarythistle.com>
2026-01-29 10:00:15 +0000

    add model and dbcontext

---
+++ marketplacer/Models/Seller.cs
@@ -0,0 +1,10 @@
+public class Seller
+{
+    public int Id { get; set; }
+
+    public required string SellerName { get; set; }
+
+    public string? SellerDomain { get; set; }
+
+    public string? HubSpotId { get; set; }
+}
